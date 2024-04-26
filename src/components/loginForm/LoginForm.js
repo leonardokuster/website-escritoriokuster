d868de0ab6ad5290b0633192f354b01852a04bdc
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2024-04-26 15:01:13 -0300

    .

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -3,7 +3,7 @@ import { useFormik } from "formik";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import * as yup from 'yup';
-import styles from '../loginform/loginform.module.css';
+import styles from '../loginForm/loginform.module.css';
 import Link from 'next/link';
 import axios from 'axios';
 import { motion } from "framer-motion";
