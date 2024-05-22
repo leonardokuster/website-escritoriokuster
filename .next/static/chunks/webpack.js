eeec8d5ab7e7c17682c14190d6cf10aa2016dcb6
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2024-05-22 15:05:45 -0300

    Ajustes CSS

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "40417793819538ba"; }
+/******/ 		__webpack_require__.h = function() { return "27cf5903c27e9a50"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/dashboard/screens/settings.js
+++ src/dashboard/screens/settings.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import styles from '../styles/settings.module.css';
+import InProgress from './inProgress';
 
 export default function Settings() {
 
     return (
-        <div>
+        <div className={styles.settings}>
             <h1>Configurações!</h1>
+            <InProgress />
         </div>
     );
 }
