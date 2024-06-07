60755435917e2fef5f8703856033f9b3ed449e66
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2024-06-07 11:57:56 -0300

    Instalação do Hotjar

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,6 +16,15 @@ export default function ContactPage() {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-4CQLGTWT49');
+
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5016243,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
             `,
           }}
         />
